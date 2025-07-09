0b7f881ae0595af917f2fdebd874d9811ccf33bb
Rajni <rajnisandhu487@gmail.com>
2025-07-09 16:07:48 -0400

    Fix: safelist classes for modal buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    'lg:flex-row',
+    'flex-col',
+    'gap-4',
+    'flex-1',
+    'px-6',
+    'py-2',
+    'rounded-xl',
+    'text-center',
+    'bg-gray-700',
+    'hover:bg-[#94d58b]',
+    'text-gray-200'
+  ],
   theme: {
     extend: {
-        backgroundImage: {
-        "skills-gradient": "linear-gradient(90deg, #bae9b4 10%, #f5d8d7 50%, #bae9b4 50%, #f5d8d7 100%)",
-        "project-gradient": "linear-gradient(135deg, #ff9a9e 0%, #bff6d2 100%)",
+      backgroundImage: {
+        "skills-gradient":
+          "linear-gradient(90deg, #bae9b4 10%, #f5d8d7 50%, #bae9b4 50%, #f5d8d7 100%)",
+        "project-gradient":
+          "linear-gradient(135deg, #ff9a9e 0%, #bff6d2 100%)",
       },
     },
   },
-
   plugins: [],
 };
